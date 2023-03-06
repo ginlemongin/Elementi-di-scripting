401b33ae940c2674ea3183c354ffa39532550a50
ginlemongin <davide.morolli@enaipriminifad.org>
2023-03-06 23:44:18 +0100

    esercizi

--- Lezione3/Esercizi.js
+++ Lezione3/Esercizi.js
@@ -1,10 +1,11 @@
-/**Esercitazione 1
+/**
+ Esercitazione 1
 
 Scrivere un blocco di codice Javascript che
 stampi a console i primi dieci interi pari compresi
 tra 20 e 0, partendo da 20. Importante non
 bisogna usare delle costanti, ma solo le
-istruzioni if e for. */
+istruzioni if e for.
 
 for (i = 20; i > 0; i--) {
 
@@ -15,13 +16,14 @@ for (i = 20; i > 0; i--) {
     }
 
 }
+*/
+
 
 /**Esercitazione 2
 
 Scrivere un blocco di codice Javascript che
 stampi a console i primi dieci numeri interi,
-escluso lo zero, in lingua italiana. */
-
+escluso lo zero, in lingua italiana. 
 for (i = 20; i > 0; i--) {
 
     if (i % 2 == 0 && i !== 0) {
@@ -76,4 +78,17 @@ for (i = 20; i > 0; i--) {
 
     }
 
+}
+*/
+
+/*
+Esercitazione 3
+
+Scrivere un blocco di codice Javascript che dato
+un numero stampi la tabellina corrispondente.
+ */
+
+for (let i = 0; i < array.length; index++) {
+    const element = array[index];
+    
 }
