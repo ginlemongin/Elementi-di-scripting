cfb6ba207db9d1071bc66a9142e0ab8faeed4dfd
ginlemongin <davide.morolli@enaipriminifad.org>
2023-03-10 17:47:07 +0100

    Revert "esercizi rubrica"
    This reverts commit 2f4626b5038786f1301b9a7fef6dfad3a1589849.

---
+++ lezione 6/init-data.js
@@ -0,0 +1,9 @@
+let contatto = new Contatto("Giuseppe", "Rossi", "05412345678");
+let contatto2 = new Contatto("Giuseppe", "Rossini", "054123456789");
+let contatto21 = new Contatto("Giuseppe", "Rossini", "054123456789");
+
+let rubrica = new Rubrica();
+rubrica.aggiungiContatto(contatto);
+rubrica.aggiungiContatto(contatto2);
+rubrica.aggiungiContatto(contatto21);
+
