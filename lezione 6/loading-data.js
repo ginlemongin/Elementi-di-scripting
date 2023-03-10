c9db2df6d15de8c00bea13f4d53326e8fd7f9fe6
ginlemongin <davide.morolli@enaipriminifad.org>
2023-03-10 11:28:03 +0100

    esercizio rubrica

---
+++ lezione 6/loading-data.js
@@ -0,0 +1,29 @@
+/*per contenere i div dell'html */
+function creaDivColonne(valoreT){
+    let divCol = document.createElement("div");
+    divCol.className = "col-3";
+    /*dopo aver creato la classe dobbiamo mettere il valore testo che hai in input*/
+    divCol.innerHTML = valoreTesto;
+    return divCol;
+}
+
+/*ogni contatto rappresenta una riga quindi facciamo le righe*/
+function creaDivRighe(indice){
+    let divRow = document.createElement("div");
+    divRow.setAttribute("data-id", indice);
+    divRow.className = "row";
+    return divRow;
+}
+
+/*questo elemnto rappresenta i ldiv che farà da contenitore per le mie righe */
+let divRowRoot = document.getElementById("rubrica-row");
+let listaContatti = rubrica.stampaContatti("az");
+
+for (let index = 0; index < listaContatti.length; index++) {
+    const contatto = listaContatti[index];
+    let divRiga = creaDivRighe(index);
+    divRiga.appendChild(creaDivColonne(index));
+    divRiga.appendChild(creaDivColonne(contatto.nome));
+    divRiga.appendChild(creaDivColonne(contatto.cognome));
+    divRiga.appendChild(creaDivColonne(contatto.telefono));
+}
