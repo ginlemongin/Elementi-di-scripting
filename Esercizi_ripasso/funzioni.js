75e66deb8541760060df8644fb0f3b2e82acb229
ginlemongin <davide.morolli@enaipriminifad.org>
2023-03-11 09:35:29 +0100

    eh

---
+++ Esercizi_ripasso/funzioni.js
@@ -0,0 +1,10 @@
+/*
+Scrivi una funzione che prenda in input un numero e
+restituisca TRUE se è un numero primo, FALSE
+altrimenti.
+Scrivi una seconda funzione, che prenda in input un
+numero N e stampi i primi N numeri primi.
+Esempio:
+Input: n = 5
+Output: true 2 3 5 7 11
+*/
